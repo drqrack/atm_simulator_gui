3a7c31855cbc9489a80b59e0b50aac314670da26
drqrack <chrisappia461@gmail.com>
2025-08-20 10:37:27 +0000

    changes

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ root = tk.Tk()
 
 # Add a title to main window
 root.title("ATM Simulator")
-# Specify main window size
-root.geometry("500x400")
+
+# Create a tk frame for main window
+frame = tk.Frame(root, padx=50, pady=50)
+frame.pack()
 
 # Add account number entry
-account_number_entry = tk.Entry(root, width=50)
-account_number_entry.pack(side='top')
+account_number_entry = tk.Entry(frame, width=15)
+# account_number_entry.pack(side='top')
+account_number_entry.grid(column=1, row=1)
 
 # Add check balance button
-check_balance_btn = tk.Button(root, text="Check Balance", width='15', height='2', command=lambda:check_balance(account_number_entry.get()))
-check_balance_btn.pack(side='top')
+check_balance_btn = tk.Button(frame, text="Check Balance", width='15', height='2', command=lambda:check_balance(account_number_entry.get()))
+# check_balance_btn.pack(side='top')
+check_balance_btn.grid(column=1, row=2)
 
 # Add deposit button
-deposit_btn = tk.Button(root, text="Deposit", width='15', height='2')
-deposit_btn.pack(side='top')
+deposit_btn = tk.Button(frame, text="Deposit", width='15', height='2')
+# deposit_btn.pack(side='top')
+deposit_btn.grid(column=2, row=2)
 
 # Open the main window
 root.mainloop()
